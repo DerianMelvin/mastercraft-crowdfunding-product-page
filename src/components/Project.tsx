c6c814a15d8179a6a298ffeb019b2bc0a16ccea2
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-09 17:49:51 +0800

    Import component Status

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,9 +1,11 @@
+import Status from "./project/Status";
 import Title from "./project/Title";
 
 const Project = () => {
   return (
     <main>
       <Title />
+      <Status />
     </main>
   );
 };
