ecb10b7af9c5a384bf2d8d986a3e42484d2dfb27
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-09 16:56:38 +0800

    Create component Title - Import logo-mastercraft.svg, icon-bookmark.svg

---
+++ src/components/project/Title.tsx
@@ -0,0 +1,25 @@
+import logoMastercraft from "../../images/logo-mastercraft.svg";
+import iBookmark from "../../images/icon-bookmark.svg";
+
+const Title = () => {
+  return (
+    <div>
+      <div>
+        <img src={logoMastercraft} alt="mastercraft logo" />
+        <h1>Mastercraft Bamboo Monitor Riser</h1>
+        <p>
+          A beautiful & handcrafted monitor stand to reduce neck and eye strain.
+        </p>
+      </div>
+
+      <div>
+        <button>Back this project</button>
+        <button>
+          <img src={iBookmark} alt="bookmark icon" /> <span>Bookmark</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Title;
