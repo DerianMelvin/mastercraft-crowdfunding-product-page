2a03c2369faa0499e6e8321e953f653e591364b8
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-09 17:49:34 +0800

    Create component Status - Import useState

---
+++ src/components/project/Status.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const Status = () => {
+  const [funds, setFunds] = useState(89914);
+  const [backers, setBackers] = useState(5007);
+  const [days, setDays] = useState(56);
+
+  // Format numbers to decimal as strings
+  const toDecimalString = (num: number): string => {
+    return new Intl.NumberFormat("en-US", {
+      style: "decimal",
+    }).format(num);
+  };
+
+  const formattedFunds: string = toDecimalString(funds);
+  const formattedBackers: string = toDecimalString(backers);
+
+  return (
+    <div>
+      <div>
+        <h2>${formattedFunds}</h2>
+        <p>of $100,000 backed</p>
+      </div>
+      <div>
+        <h2>{formattedBackers}</h2>
+        <p>total backers</p>
+      </div>
+      <div>
+        <h2>{days}</h2>
+        <p>days left</p>
+      </div>
+    </div>
+  );
+};
+
+export default Status;
