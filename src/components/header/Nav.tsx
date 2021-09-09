880b375d436eb7fe202dcf7cfb64948570a9a124
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-09 16:36:03 +0800

    Create component Nav

---
+++ src/components/header/Nav.tsx
@@ -0,0 +1,19 @@
+const Nav = (): JSX.Element => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#link">About</a>
+        </li>
+        <li>
+          <a href="#link">Discover</a>
+        </li>
+        <li>
+          <a href="#link">Get Started</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
