6634f8a0f873c68e765e3c0539016c5d5df5a2cf
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-11 12:28:43 +0800

    Create & import CSS module Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import logo from "../images/logo.svg";
 import Nav from "./header/Nav";
+import headerStyles from "../css/Header.module.css";
 
 const Header = (): JSX.Element => {
   return (
-    <header>
+    <header className={headerStyles.header}>
       <img src={logo} alt="crowdfund logo" />
       <Nav />
     </header>
