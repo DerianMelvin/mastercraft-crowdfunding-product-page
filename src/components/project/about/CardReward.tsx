866a46ac478c80e331c7ad33cbd93afa5fbf7973
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-13 17:12:56 +0800

    Import CSS module Card

--- src/components/project/about/CardReward.tsx
+++ src/components/project/about/CardReward.tsx
@@ -1,3 +1,5 @@
+import cardStyles from '../../../css/Card.module.css';
+
 type Props = {
   reward: string;
   description: string;
@@ -12,17 +14,19 @@ const CardReward = ({
   fundingLimit,
 }: Props): JSX.Element => {
   return (
-    <div>
-      <div>
-        <h3>{reward}</h3>
-        <span>Pledge ${pledgeAmount} or more</span>
-      </div>
-      <p>{description}</p>
-      <div>
-        <h2>
-          {fundingLimit} <span>left</span>
-        </h2>
-        <button>Select Reward</button>
+    <div className={cardStyles.card}>
+      <div className={cardStyles.content}>
+        <div className={cardStyles.title}>
+          <h3>{reward}</h3>
+          <span>Pledge ${pledgeAmount} or more</span>
+        </div>
+        <p className={cardStyles.description}>{description}</p>
+        <div className={cardStyles.information}>
+          <h2>
+            {fundingLimit} <span>left</span>
+          </h2>
+          <button className={cardStyles.btn}>Select Reward</button>
+        </div>
       </div>
     </div>
   );
