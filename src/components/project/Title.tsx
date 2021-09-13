e73c3b4dc32276d5f9367900bb01c9cb7c48ba1d
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-13 13:05:54 +0800

    Adjust HTML element

--- src/components/project/Title.tsx
+++ src/components/project/Title.tsx
@@ -5,20 +5,22 @@ import projectStyles from "../../css/Project.module.css";
 const Title = (): JSX.Element => {
   return (
     <div className={projectStyles.container}>
-      <div className={projectStyles.title}>
-        <img src={logoMastercraft} alt="mastercraft logo" />
-        <h1>Mastercraft Bamboo Monitor Riser</h1>
-        <p>
-          A beautiful & handcrafted monitor stand to reduce neck and eye strain.
-        </p>
-      </div>
-
-      <div className={projectStyles.callToActions}>
-        <button className={projectStyles.btn}>Back this project</button>
-        <button className={projectStyles.bookmark}>
-          <img src={iBookmark} alt="bookmark icon" /> <span>Bookmark</span>
-        </button>
-      </div>
+      <section>
+        <div className={projectStyles.title}>
+          <img src={logoMastercraft} alt="mastercraft logo" />
+          <h1>Mastercraft Bamboo Monitor Riser</h1>
+          <p>
+            A beautiful & handcrafted monitor stand to reduce neck and eye strain.
+          </p>
+        </div>
+        
+        <div className={projectStyles.callToActions}>
+          <button className={projectStyles.btn}>Back this project</button>
+          <button className={projectStyles.bookmark}>
+            <img src={iBookmark} alt="bookmark icon" /> <span>Bookmark</span>
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
