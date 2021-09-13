a13ff589109f1c6924f681e7c8e3b734bccb2028
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-13 13:05:34 +0800

    Import CSS module Project - Adjust HTML elements

--- src/components/project/Status.tsx
+++ src/components/project/Status.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import projectStyles from "../../css/Project.module.css";
 
 const Status = (): JSX.Element => {
   const [funds, setFunds] = useState(89914);
@@ -16,19 +17,21 @@ const Status = (): JSX.Element => {
   const formattedBackers: string = toDecimalString(backers);
 
   return (
-    <div>
-      <div>
-        <h2>${formattedFunds}</h2>
-        <p>of $100,000 backed</p>
-      </div>
-      <div>
-        <h2>{formattedBackers}</h2>
-        <p>total backers</p>
-      </div>
-      <div>
-        <h2>{days}</h2>
-        <p>days left</p>
-      </div>
+    <div className={projectStyles.container}>
+      <section className={projectStyles.status}>
+        <div className={projectStyles.statusInfo}>
+          <h2>${formattedFunds}</h2>
+          <p>of $100,000 backed</p>
+        </div>
+        <div className={projectStyles.statusInfo}>
+          <h2>{formattedBackers}</h2>
+          <p>total backers</p>
+        </div>
+        <div className={projectStyles.statusInfo}>
+          <h2>{days}</h2>
+          <p>days left</p>
+        </div>
+      </section>
     </div>
   );
 };
