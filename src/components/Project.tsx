634591e4136e51edf83422477130ca5dedbc4b50
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-14 18:02:30 +0800

    Import & add state to view component Modal - Import useState - Import component Modal - Add props to component Title, About, Modal

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import About from "./project/About";
 import Status from "./project/Status";
 import Title from "./project/Title";
 import projectStyles from "../css/Project.module.css";
+import Modal from "./Modal";
 
 const Project = (): JSX.Element => {
+  const [viewModal, setViewModal] = useState<boolean>(false);
+
+  const handleModal = () => {
+    setViewModal((prevState) => !prevState);
+  };
+
   return (
     <main className={projectStyles.main}>
-      <Title />
+      {viewModal && <Modal handleModal={handleModal} />}
+
+      <Title handleModal={handleModal} />
       <Status />
-      <About />
+      <About handleModal={handleModal} />
     </main>
   );
 };
