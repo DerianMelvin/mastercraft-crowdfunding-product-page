ae4d08f0397cec3183e035b80a082fae6b31c696
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-18 17:20:49 +0800

    Add state & function to handle radio events - Import ChangeEvent, useState - Add props to component CardSelect

--- src/components/ModalSelect.tsx
+++ src/components/ModalSelect.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import modalStyles from "../css/Modal.module.css";
 import CardSelect from "./modal/CardSelect";
 
@@ -6,6 +7,12 @@ type Props = {
 };
 
 const ModalSelect = ({ handleModal }: Props): JSX.Element => {
+  const [radioName, setRadioName] = useState<string>("");
+
+  const handleRadioChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setRadioName(e.target.value);
+  };
+
   return (
     <div className={modalStyles.overlay}>
       <div className={modalStyles.container}>
@@ -21,24 +28,32 @@ const ModalSelect = ({ handleModal }: Props): JSX.Element => {
           <CardSelect
             rewardTitle="Pledge with no reward"
             description="Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email."
+            radioName={radioName}
+            handleRadioChange={handleRadioChange}
           />
           <CardSelect
             rewardTitle="Bamboo Stand"
             description="You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list."
             minimumPledge={25}
             fundingLimit={101}
+            radioName={radioName}
+            handleRadioChange={handleRadioChange}
           />
           <CardSelect
             rewardTitle="Black Edition Stand"
             description="You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included."
             minimumPledge={75}
             fundingLimit={64}
+            radioName={radioName}
+            handleRadioChange={handleRadioChange}
           />
           <CardSelect
             rewardTitle="Mahogany Special Edition"
             description="You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included."
             minimumPledge={200}
             fundingLimit={0}
+            radioName={radioName}
+            handleRadioChange={handleRadioChange}
           />
         </div>
       </div>
