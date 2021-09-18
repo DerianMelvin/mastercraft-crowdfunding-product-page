33b10dc569f90beb73951dfefb6a53886071007e
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-18 17:34:32 +0800

    Add & adjust returned element - Import ChangeEvent from react - Add props radioName, handleRadioChange - Adjust function return type - Add function renderPledgeAmount - Adjust input attributes

--- src/components/modal/CardSelect.tsx
+++ src/components/modal/CardSelect.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent } from "react";
 import cardStyles from "../../css/Card.module.css";
 
 type Props = {
@@ -5,19 +6,25 @@ type Props = {
   description: string;
   minimumPledge?: number;
   fundingLimit?: number;
+  radioName: string;
+  handleRadioChange(e: ChangeEvent<HTMLInputElement>): void;
 };
+
+type RenderElementType = JSX.Element | string;
 
 const CardSelect = ({
   rewardTitle,
   description,
   minimumPledge,
   fundingLimit,
+  radioName,
+  handleRadioChange,
 }: Props): JSX.Element => {
-  const renderMinimumPledge = (): JSX.Element | string => {
+  const renderMinimumPledge = (): RenderElementType => {
     return minimumPledge ? <span>Pledge ${minimumPledge} or more</span> : "";
   };
 
-  const renderFundingLimit = (): JSX.Element | string => {
+  const renderFundingLimit = (): RenderElementType => {
     return fundingLimit || fundingLimit === 0 ? (
       <h2>
         {fundingLimit} <span>left</span>
@@ -27,10 +34,35 @@ const CardSelect = ({
     );
   };
 
+  const renderPledgeAmount = (): RenderElementType => {
+    return radioName === rewardTitle ? (
+      <div>
+        <span>Enter your pledge</span>
+        <div>
+          <input
+            type="number"
+            name="pledgeAmount"
+            value={minimumPledge ? minimumPledge : 1}
+            min={minimumPledge ? minimumPledge : 1}
+          />
+          <button>Continue</button>
+        </div>
+      </div>
+    ) : (
+      ""
+    );
+  };
+
   return (
     <div className={cardStyles.cardSelect}>
       <div className={cardStyles.selection}>
-        <input type="radio" name="pledge" id={rewardTitle} />
+        <input
+          type="radio"
+          name="pledge"
+          value={rewardTitle}
+          onChange={handleRadioChange}
+          checked={radioName === rewardTitle}
+        />
         <div className={cardStyles.selectionGroup}>
           <div className={cardStyles.title}>
             <h3>{rewardTitle}</h3>
@@ -43,6 +75,7 @@ const CardSelect = ({
         <p>{description}</p>
         {renderFundingLimit()}
       </div>
+      {renderPledgeAmount()}
     </div>
   );
 };
